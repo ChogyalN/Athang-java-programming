318efabc21c7b64b4b459de6a1a50b11eeed3a86
ChogyalN <c.norbu@yangkhor.com>
2024-03-22 15:41:27 +0600

    exception-commit

---
+++ JavaFrontendAthang/src/exceptions/ExceptionMainClass.java
@@ -0,0 +1,23 @@
+package exceptions;
+
+import java.util.Scanner;
+
+public class ExceptionMainClass {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number between 0-9: ");
+        int num = 0;
+        try {
+            num = sc.nextInt();
+            if (num > 9 || num < 0) {
+                throw new Exception("The number should be between 0 - 9");
+            }
+            System.out.println("The entered number is " + num);
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Please enter a valid number!");
+            num = sc.nextInt();
+        }
+
+    }
+}
